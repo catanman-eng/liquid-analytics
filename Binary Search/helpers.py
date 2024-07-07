1b7d3a829ddac5cdbb2f74706c94b3a5a44419d6
evancatania <evan.catania@treeoflife.com>
2024-07-07 17:44:47 -0400

    Moved binary function to helper module, basic unit test

---
+++ Binary Search/helpers.py
@@ -0,0 +1,17 @@
+from typing import List
+
+def binary_search(target: int, arr: List[int]) -> int:
+    left = 0
+    right = len(arr) - 1
+
+    while left <= right:
+        mid = left + (right - left) // 2
+
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+
+    return -1
--- Binary Search/main.py
+++ Binary Search/main.py
@@ -1,23 +1,6 @@
-from typing import List
-
-
-def binary_search(target: int, arr: List[int]) -> int:
-    left = 0
-    right = len(arr) - 1
-
-    while left <= right:
-        mid = left + (right - left) // 2
-
-        if arr[mid] == target:
-            return mid
-        elif arr[mid] < target:
-            left = mid + 1
-        else:
-            right = mid - 1
-
-    return -1
+import helpers
 
 if __name__ == "__main__":
     arr = [1, 3, 5, 7, 9]
     target = 5
-    print(binary_search(target, arr))  # Output: 2
+    print(helpers.binary_search(target, arr))  # Output: 2
