b35f404d10b0732c054e6bf7f55461db4e514888
evancatania <evan.catania@treeoflife.com>
2024-07-07 19:15:40 -0400

    Added setUp method to testing file

--- Binary Search/test-binary.py
+++ Binary Search/test-binary.py
@@ -2,6 +2,10 @@ import helpers
 import unittest
 
 class TestCalculations(unittest.TestCase):
+  
+  @classmethod
+  def setUp(self) -> None:
+    self.array = [1, 3, 5, 7, 9]
 
   def test_empty_array(self):
     array = []
@@ -9,24 +13,20 @@ class TestCalculations(unittest.TestCase):
     self.assertEqual(helpers.binary_search(target, array), -1) 
 
   def test_target_not_in_array(self):
-    array = [1, 3, 5, 7, 9]
     target = 6
-    self.assertEqual(helpers.binary_search(target, array), -1)
+    self.assertEqual(helpers.binary_search(target, self.array), -1)
 
   def test_target_at_end_of_array(self):
-    array = [1, 3, 5, 7, 9]
     target = 9
-    self.assertEqual(helpers.binary_search(target, array), 4)
+    self.assertEqual(helpers.binary_search(target, self.array), 4)
   
   def test_target_at_start_of_array(self):  
-    array = [1, 3, 5, 7, 9]
     target = 1
-    self.assertEqual(helpers.binary_search(target, array), 0)
+    self.assertEqual(helpers.binary_search(target, self.array), 0)
 
   def test_target_at_middle_of_array(self):
-    array = [1, 3, 5, 7, 9]
     target = 5
-    self.assertEqual(helpers.binary_search(target, array), 2)
+    self.assertEqual(helpers.binary_search(target, self.array), 2)
 
   def test_same_value_array(self):
       array = [1, 1, 1, 1, 1]
