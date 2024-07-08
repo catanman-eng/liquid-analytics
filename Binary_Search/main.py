2ddce5301de9553ed149eeff052d61df5c69b579
evancatania <evan.catania@gmail.com>
2024-07-07 23:10:47 -0400

    small logging add

--- Binary_Search/main.py
+++ Binary_Search/main.py
@@ -2,10 +2,13 @@ import helpers
 
 if __name__ == "__main__":
     algorithms = ["binary_search", "linear_search", "ternary_search"]
-    # Sorted array of length 1000000
-    sorted_array = [i for i in range(1000000)]
+
+    # Sorted array of length n
+    n = 10000000
+    sorted_array = [i for i in range(n)]
+    target = 5
 
     # Perform search for each algorithm
-    target = 5
+    print(f"Target: {target} in sorted array of length {n}\n")
     for algo in algorithms:
         helpers.run_search_algorithm(algo, target, sorted_array)
