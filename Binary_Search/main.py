3a9122eeb768fc1c54031ffb1c07aa19d3879566
evancatania <evan.catania@gmail.com>
2024-07-07 22:53:47 -0400

    Added ternary and linear search, timer logic

--- Binary_Search/main.py
+++ Binary_Search/main.py
@@ -1,6 +1,11 @@
 import helpers
 
 if __name__ == "__main__":
-    arr = [10, 25, 1, -2, 0]
-    target = 1
-    print(helpers.binary_search(target, arr))  # Output: 2
+    algorithms = ["binary_search", "linear_search", "ternary_search"]
+    # Sorted array of length 1000000
+    sorted_array = [i for i in range(1000000)]
+
+    # Perform search for each algorithm
+    target = 5
+    for algo in algorithms:
+        helpers.run_search_algorithm(algo, target, sorted_array)
