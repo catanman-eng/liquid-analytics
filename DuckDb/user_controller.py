02cce84f38090e7cef479f89254c376aa7b15208
evancatania <evan.catania@treeoflife.com>
2024-07-17 23:15:29 -0400

    added user controller

---
+++ DuckDb/user_controller.py
@@ -0,0 +1,26 @@
+class UserController:
+  def __init__(self, con):
+    self.con = con
+    self.create_users_table()
+
+  def create_users_table(self):
+    self.con.execute("CREATE TABLE IF NOT EXISTS users (username VARCHAR)")
+  
+  def check_user_exists(self, username):
+    result = self.con.execute(f"SELECT * FROM users WHERE username = '{username}'")
+    return len(result.fetchdf()) > 0
+  
+  def add_user(self, username):
+    if not self.check_user_exists(username):
+      self.con.execute(f"INSERT INTO users VALUES ('{username}')")
+      return True
+    else:
+      print(f"User {username} already exists")
+    return False
+  
+  def remove_user(self, username):
+    self.con.execute(f"DELETE FROM users WHERE username = '{username}'")
+  
+  def get_users(self):
+    result = self.con.execute("SELECT * FROM users")
+    return result.fetchdf()
