b99086f2ee7cf710bbfa08940d1b06f5fee36f74
evancatania <evan.catania@treeoflife.com>
2024-07-19 01:15:39 -0400

    using pydantic, added config_id to user table

--- DuckDb/controllers/user_config_controller.py
+++ DuckDb/controllers/user_config_controller.py
@@ -1,5 +1,13 @@
 from typing import Callable
 from controllers.helpers import Helper
+from controllers.user_controller import UserController
+from pydantic import BaseModel
+
+
+class UserConfig(BaseModel):
+    user_id: str
+    kelly_multiplyer: float
+    bankroll: int
 
 
 def handle_database_errors(func: Callable[..., any]) -> Callable[..., any]:
@@ -16,25 +24,60 @@ class UserConfigController:
     def __init__(self, con):
         self.con = con
         self.utils = Helper()
+        self.config = ["kelly_multiplyer", "bankroll"]
         self.create_user_config_table()
+        self.user_controller = UserController(con)
 
-    @handle_database_errors
-    def create_config_table(self):
-        self.con.execute("""
-              CREATE TABLE IF NOT EXISTS config(
-                id VARCHAR PRIMARY KEY,
-                name VARCHAR
-              )
-          """)
-        
     @handle_database_errors
     def create_user_config_table(self):
         self.con.execute("""
               CREATE TABLE IF NOT EXISTS user_config(
-                user_id VARCHAR, 
-                config_id VARCHAR,
-                PRIMARY KEY (config_id, user_id),
-                FOREIGN KEY (config_id) REFERENCES config(id),
+                user_id VARCHAR PRIMARY KEY,
+                kelly_multiplyer DOUBLE,
+                bankroll INTEGER
                 FOREIGN KEY (user_id) REFERENCES users(id)
               )
           """)
+
+    @handle_database_errors
+    def check_config_exists(self, username):
+        user_id = self.user_controller.get_user_id(username)
+        result = self.con.execute(
+            "SELECT * FROM user_config WHERE user_id = ?", [user_id]
+        )
+        return len(result.fetchdf()) > 0
+
+    @handle_database_errors
+    def get_all_configs(self):
+        return self.con.sql("SELECT * FROM user_config")
+
+    @handle_database_errors
+    def get_user_config(self, username):
+        user_id = self.user_controller.get_user_id(username)
+        result = self.con.execute(
+            "SELECT * FROM user_config WHERE user_id = ?", [user_id]
+        )
+        return result.fetchdf()
+
+    @handle_database_errors
+    def add_user_config(self, username, kelly_multiplyer, bankroll):
+        user_id = self.user_controller.get_user_id(username)
+
+        config = UserConfig(
+            user_id=user_id, kelly_multiplyer=kelly_multiplyer, bankroll=bankroll
+        )
+
+        result = self.con.execute(
+            "SELECT * FROM user_config WHERE user_id = ?", [config.user_id]
+        )
+        if len(result.fetchdf()) > 0:
+            self.con.execute(
+          "UPDATE user_config SET kelly_multiplyer = ?, bankroll = ? WHERE user_id = ?",
+          [config.kelly_multiplyer, config.bankroll, config.user_id],
+            )
+        else:
+            self.con.execute(
+          "INSERT INTO user_config VALUES (?, ?, ?)",
+          [config.user_id, config.kelly_multiplyer, config.bankroll],
+            )
+        print(f"Config updated for user {username} successfully")
